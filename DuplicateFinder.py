7ad562f07f475cc1703b2da39f342fc734394f2e
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-12-17 13:31:52 +0300

    counterDuplicate

--- DuplicateFinder.py
+++ DuplicateFinder.py
@@ -36,6 +36,7 @@ folder_selected = filedialog.askdirectory()
 
 counter = 0
 fileList =[]
+duplicateList =[]
 for root, dir, files in os.walk(folder_selected):
     for file in files:
         filePath = root +'/'+str(file)
@@ -45,8 +46,22 @@ for root, dir, files in os.walk(folder_selected):
 
 
 
+print('Total Files : ',len(fileList))
+counterDuplicate = 0
+for x in range(len(fileList)):
+    for y in range(len(fileList)):
+        if y+x+1 < len(fileList):
+            z = y+x+1
+        else:
+            break
+        print(fileList[x], ' VS ', fileList[z])
+        if fileList[x][1] == fileList[z][1]:
+            duplicateList.append(fileList[x])
+            duplicateList.append(fileList[z])
+            counterDuplicate = counterDuplicate+1
 
 
 
 
-
+print('the result for all duplicate files : ', duplicateList)
+print('Total Files : ',counterDuplicate)
